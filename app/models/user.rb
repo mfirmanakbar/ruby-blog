68e0679faf90d233098da6fdfa1b24b690fdfb5a
mfirmanakbar <mu.firman.akbar@gmail.com>
2019-05-22 20:31:31 +0700

    set validation to user table
      # Validation
      # username & email must be present and unique
      # validate email format using regex
      # case_sensitive: false is between, can't save if username (Firman and firMaN)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ApplicationRecord
-  
+
+  # Validation
+  # username & email must be present and unique
+  # validate email format using regex
+  # case_sensitive: false is between, can't save if username (Firman and firMaN)
+
+  validates :username,
+  presence: true, uniqueness: {case_sensitive: false}, length: { minimum: 3, maximum: 25 }
+
+  # tested on https://rubular.com/
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,
+  presence: true, uniqueness: {case_sensitive: false}, length: { minimum: 3, maximum: 105 },
+  format: { with: VALID_EMAIL_REGEX }
+
 end
