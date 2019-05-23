5d715c35a48eae5f01a5f12e86f23698819f203e
mfirmanakbar <mu.firman.akbar@gmail.com>
2019-05-23 09:05:42 +0700

    login

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,8 @@ Rails.application.routes.draw do
   #it means we already difined get 'signup', to: 'users#new'
   resources :users, except: [:new]
 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
 end
